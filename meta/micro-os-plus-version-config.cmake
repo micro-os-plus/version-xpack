f364709d88149a6598c49b3e65bd38846ef0b2a4
Liviu Ionescu <ilg@livius.net>
2021-02-25 16:46:46 +0200

    cmake: simplify, keep only config.cmake

--- meta/micro-os-plus-version-config.cmake
+++ meta/micro-os-plus-version-config.cmake
@@ -12,11 +12,40 @@
 # https://cmake.org/cmake/help/v3.18/
 # https://cmake.org/cmake/help/v3.18/manual/cmake-packages.7.html#package-configuration-file
 
-# message(STATUS "Including micro-os-plus-version-config...")
+if(micro-os-plus-version-included)
+  return()
+endif()
+
+set(micro-os-plus-version-included TRUE)
+
+message(STATUS "Including micro-os-plus-version...")
+
+# -----------------------------------------------------------------------------
+# The current folder.
+
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
 # -----------------------------------------------------------------------------
 
-include("${CMAKE_CURRENT_LIST_DIR}/xpack-helper.cmake")
+if (NOT TARGET micro-os-plus-version-interface)
+
+  add_library(micro-os-plus-version-interface INTERFACE EXCLUDE_FROM_ALL)
+
+  # ---------------------------------------------------------------------------
+
+  target_include_directories(
+    micro-os-plus-version-interface
+
+    INTERFACE
+      ${xpack_current_folder}/include
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(micro-os-plus::version ALIAS micro-os-plus-version-interface)
+  message(STATUS "micro-os-plus::version")
+
+endif()
 
 # -----------------------------------------------------------------------------
-
