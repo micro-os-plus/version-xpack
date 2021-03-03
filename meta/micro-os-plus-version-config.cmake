714828fb1d54d3cad1f83ca0a5afbc9b70aa8632
Liviu Ionescu <ilg@livius.net>
2021-03-04 00:36:44 +0200

    cmake: add xpack_process_package_version()

--- meta/micro-os-plus-version-config.cmake
+++ meta/micro-os-plus-version-config.cmake
@@ -18,7 +18,7 @@ endif()
 
 set(micro-os-plus-version-included TRUE)
 
-message(STATUS "Including micro-os-plus-version...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # The current folder.
